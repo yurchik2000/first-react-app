835e70c09c9ecea1d30b6fc873a762503fb1ce2b
Galunko <d:\Mail>
2022-05-14 20:16:41 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { DateTime } from "luxon";
+import React from 'react';
 
-function App() {
+function App() {  
+  const [date, setDate] = React.useState();
+  const [state, setState] = React.useState({ show: true });
+  const [count, setCount] = React.useState(0);
+
+  function updateDate() {
+    return setDate(new Date().toISOString());
+  }
+  function updateCounter() {
+    return setCount(count + 1);
+  }
+  
+  function showMessage() {    
+    return setState({ show: !state.show});
+  }
+  
+  let content = "";
+  if (state.show)  content = ""    
+    else content = new Date().toISOString();        
+  //console.log(state.show, content);        
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">            
+      <button onClick={updateCounter}>Counter</button>
+      <h3>{count}</h3>
+      <button onClick={showMessage}>Show Date</button>
+      <h3>{content}</h3>
+    </div>    
   );
 }
 
